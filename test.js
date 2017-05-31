37fd0a70c3fb8ffdecb3a0b1ed8358b2afe94d34
EdozdeDandez <edith.edidan.dande@gmail.com>
2017-05-31 13:51:38 +0000

    First commit

---
+++ test.js
@@ -0,0 +1,31 @@
+'use strict';
+var family = (function(){
+	function People(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	function Fam(name, age, relation){
+		People.call(this, name, age);
+		this.relation = relation;
+	}
+	var nfam;
+	return {
+		setFam : function(name1,age1,relation1){
+			nfam = new Fam(name1,age1,relation1);
+
+		},
+		getFam : function(){
+			return "My "+ nfam.relation + " is called "+ nfam.name+" and is "+ nfam.age+" years old";
+		}
+	}
+})();
+var name = "Edith";
+console.log("My name is "+name);
+family.setFam("Ed",2,"sister");
+console.log(family.getFam());
+family.setFam("Dee",28,"brother");
+console.log(family.getFam());
+family.setFam("Dan",50,"father");
+console.log(family.getFam());
+family.setFam("Jane",45,"mother");
+console.log(family.getFam());
