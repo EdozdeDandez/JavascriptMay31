c31af2b43a24dddfcb5fca90427d23922db745dd
EdozdeDandez <edith.edidan.dande@gmail.com>
2017-06-03 21:54:54 +0000

    Js

--- test.js
+++ test.js
@@ -5,22 +5,44 @@ var family = (function(){
 		this.age = age;
 	}
 	function Fam(name, age, relation){
-		People.call(this, age, relation);
+		People.call(this, name, age);
 		this.relation = relation;
 	}
 	Fam.prototype = Object.create(People.prototype);
-	var nfam;
+	var nfam = new Fam("e",2,"Siz");
+	var allfam = [];
+	var f;
+	var name1;
+	var age1;
+	var relation1;
 	return {
-		setFam : function(name1,age1,relation1){
+		setFam : function(){
+			name1 = document.getElementById("fname").value.trim();
+			age1 = document.getElementById("age").value.trim();
+			relation1 = document.getElementById("relate").value.trim();
 			nfam = new Fam(name1,age1,relation1);
+			allfam.push(nfam);
+			console.log(name1);
+			$('#fform').trigger("reset");
+			return family.getFam();
 
 		},
 		getFam : function(){
-			return "My "+ nfam.relation + " is called "+ nfam.name+" and is "+ nfam.age+" years old";
+			var doc = document.getElementById("family-names");
+			var text = "";
+			for (var i = 0; i < allfam.length; i++) {
+				f = allfam[i];
+				text += "My " + f.relation + " is called " + f.name + " and is " + f.age + " years old.<br>";
+			}
+			return doc.innerHTML = text;
+			//return false;
+			//return "My "+ nfam.relation + " is called "+ nfam.name+" and is "+ nfam.age+" years old";
 		}
 	}
 })();
-var name = "Edith";
+/*family.setFam("Ed",2,"sister");
+console.log(family.getFam());
+/*var name = "Edith";
 console.log("My name is "+name);
 family.setFam("Ed",2,"sister");
 console.log(family.getFam());
@@ -29,4 +51,4 @@ console.log(family.getFam());
 family.setFam("Dan",50,"father");
 console.log(family.getFam());
 family.setFam("Jane",45,"mother");
-console.log(family.getFam());
+console.log(family.getFam());*/
